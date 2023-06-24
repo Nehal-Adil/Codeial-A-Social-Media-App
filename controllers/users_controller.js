f10d628537a5d82ef45fbfc011024fa52f97e01d
Nehal <mdnehaladil@gmail.com>
2023-06-24 16:05:14 +0530

    rendering the Sign-in & Sign-Up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function (req, res) {
     title: "User Profile",
   });
 };
+
+//Render the Sign Up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+//Render the Sign In page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
